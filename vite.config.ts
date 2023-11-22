ae2fcc027d4038d13f89360c68fddadf54da5461
unknown <vlad.fominov11@gmail.com>
2023-11-22 20:33:37 +0300

    ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/VladFominov/react-crud.git",
+  base: "/",
   plugins: [react()],
 });
